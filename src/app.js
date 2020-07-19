5ba4508ff05e377d6b15680aad0d84df73b1e11c
dengdengdengpan <1306631506@qq.com>
2020-07-20 00:11:41 +0800

    通过 parcel 重构按钮组件

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import WButton from './components/WButton.vue'
+
+Vue.component('w-button', WButton)
+
+new Vue({
+  el: '#app'
+})
---
+++ src/components/WButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="w-button">按钮</button>
+</template>
+
+<script>
+export default {
+  name: 'WButton'
+}
+</script>
+
+<style lang="scss">
+.w-button {
+  height: var(--button-height);
+  padding: 0 1em;
+  border: 1px solid var(--button-border-color);
+  border-radius: var(--button-border-radius);
+  background-color: var(--button-bgc);
+
+  &:hover {
+    border-color: var(--button-border-color-hover);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    background-color: var(--button-bgc-active);
+  }
+}
+</style>
