49f5bedfaa4692de6e297a181c17c75b66cbe950
dengdengdengpan <1306631506@qq.com>
2020-07-20 21:48:08 +0800

    更改 button 样式

--- src/components/WButton.vue
+++ src/components/WButton.vue
@@ -17,18 +17,22 @@ export default {
   border: 1px solid var(--button-border-color);
   border-radius: var(--button-border-radius);
   background-color: var(--button-bgc);
+  color: rgba(0, 0, 0, .65);
   cursor: pointer;
+  user-select: none;
 
-  &:hover {
-    border-color: var(--button-border-color-hover);
-  }
-
-  &:focus {
+  &, &:focus, &:active {
     outline: none;
   }
 
+  &:hover, &:focus {
+    border-color: var(--button-hover-focus);
+    color: var(--button-hover-focus);
+  }
+
   &:active {
-    background-color: var(--button-bgc-active);
+    border-color: var(--button-active);
+    color: var(--button-active);
   }
 }
 </style>
