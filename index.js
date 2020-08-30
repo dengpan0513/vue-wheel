ad933760ec8c5418b3c066607aecc2b494813f72
dengdengdengpan <1306631506@qq.com>
2020-08-30 22:54:15 +0800

    创建入口文件 index.js 以备后续 npm publish

---
+++ index.js
@@ -0,0 +1,5 @@
+import WButton from './src/components/WButton.vue'
+import WIcon from './src/components/WIcon.vue'
+import WButtonGroup from './src/components/WButtonGroup.vue'
+
+export { WButton, WIcon, WButtonGroup }
