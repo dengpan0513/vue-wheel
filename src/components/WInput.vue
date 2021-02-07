3dc2aadc36151a10e7fc21eb6cd249c464be50fb
dengdengdengpan <1306631506@qq.com>
2021-02-07 21:55:29 +0800

    重构 WInput

--- src/components/WInput.vue
+++ src/components/WInput.vue
@@ -1,80 +1,86 @@
 <template>
-  <div class="winput-wrapper">
+  <div class="w-input-wrapper">
     <input
-      class="winput"
+      class="w-input"
       v-bind="$attrs"
       :type="type"
-      :value="value"
       :readonly="readonly"
       :disabled="disabled"
+      :autofocus="autofocus"
+      :maxlength="maxLength"
     >
   </div>
 </template>
 
 <script>
+import WIcon from './WIcon.vue'
+
 export default {
-  name: 'WInput',
+  name: 'Winput',
   inheritAttrs: false,
+  components: { WIcon },
   props: {
     type: {
       type: String,
       default: 'text'
     },
-    value: {
-      type: [String, Number]
+    readonly: {
+      type: Boolean,
+      default: false
     },
     disabled: {
       type: Boolean,
       default: false
     },
-    readonly: {
+    autofocus: {
       type: Boolean,
       default: false
+    },
+    maxLength: {
+      type: [String, Number]
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-$height: 32px;
-$bgc: #fff;
-$bgc-disabled: #f5f5f5;
-$border-color: #d9d9d9;
-$border-color-hover: #40a9ff;
-$border-radius: 4px;
-$box-shadow-color: rgba(24, 144, 255, .2);
-$font-color: rgba(0, 0, 0, .85);
-$font-color-disabled: rgba(0, 0, 0, .25);
-$font-size: 14px;
+@import "../styles/variable.scss";
 
-.winput-wrapper {
+.w-input-wrapper {
   display: inline-block;
   width: 100%;
-  font-size: $font-size;
 
-  .winput {
-    width: 100%;
-    height: $height;
-    padding: 0 15px;
-    background-color: $bgc;
+  .w-input {
+    width: inherit;
+    height: 32px;
+    padding: 4px 11px;
     border: 1px solid $border-color;
-    border-radius: $border-radius;
+    background-image: none;
+    border-radius: 4px;
+    font-size: $font-size;
+
+    &::placeholder {
+      color: rgb(177, 178, 177);
+    }
 
     &:hover {
-      border-color: $border-color-hover;
+      border-color: $color-hover;
     }
 
     &:focus {
-      border-color: $border-color-hover;
+      border-color: $color-hover;
       outline: none;
-      box-shadow: 0 0 0 2px $box-shadow-color;
+      box-shadow: 0 0 0 2px rgb(24 144 255 / 20%);
     }
 
-    &[disabled] {
+    &:disabled {
       background-color: $bgc-disabled;
-      border-color: $border-color;
-      color: $font-color-disabled;
+      color: $color-disabled;
       cursor: not-allowed;
+
+      &:hover {
+        border-color: $border-color;
+      }
     }
   }
 }
