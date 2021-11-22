c1b2ef042a7c118d82a5a212c9e758b32e6ebbab
dengdengdengpan <1306631506@qq.com>
2021-11-22 22:24:58 +0800

    多个 WMessage 中有一个关闭时，实现其余实例的 top 正确

--- src/plugins/message.js
+++ src/plugins/message.js
@@ -1,7 +1,7 @@
 import WMessage from '../components/WMessage.vue'
 
 const messageList = []
-const baseGap = 16
+const gapBase = 16
 let count = 1
 
 export default {
@@ -20,22 +20,36 @@ export default {
       message.$mount()
       message.visible = true
       document.body.appendChild(message.$el)
-      let id = `w-message-${count++}`
-      message.id = id
-      message.$on('close', (instance) => {
-        messageList.forEach((item, index) => {
-          if (item.id === instance.id) {
-            messageList.splice(index, 1)
+      message.id = `w-message-${count++}`
+      message.$on('close', (currentMessage) => {
+        let index = -1
+        let heightRemoved = 0
+        messageList.forEach((item, i) => {
+          if (item.id === currentMessage.id) {
+            index = i
+            messageList.splice(i, 1)
+            heightRemoved = currentMessage.$el.offsetHeight
           }
         })
+        const length = messageList.length
+        if (length < 1 || index > length - 1) {
+          return
+        }
+        for (let i = index; i < length; i++) {
+          const messageElement = messageList[i].$el
+          const topValue = parseInt(messageElement.style.top, 10)
+          const topShould = topValue - heightRemoved - gapBase
+          console.log('topShould', topShould)
+          messageElement.style.top = topShould + 'px'
+        }
       })
       let topValue = message.top
-      messageList.forEach(item => {
-        const { offsetHeight } = item.$el
-        console.log('offsetHeight', offsetHeight)
-        topValue += offsetHeight + baseGap
-      })
-      console.log(message, topValue)
+      if (messageList.length > 0) {
+        messageList.forEach(item => {
+          const { offsetHeight } = item.$el
+          topValue += offsetHeight + gapBase
+        })
+      }
       message.top = topValue
       messageList.push(message)
     }
