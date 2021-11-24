d0659784124ed9cc2905ab2346be44b66adc0e42
dengdengdengpan <1306631506@qq.com>
2021-11-24 23:05:21 +0800

    重写 WMessage 测试

--- test/message.test.js
+++ test/message.test.js
@@ -6,40 +6,33 @@ Vue.config.devtools = false
 
 const expect = chai.expect
 
-describe('WMessage ', () => {
-  it('存在', () => {
+describe('WMessage ', function() {
+  it('存在', function() {
     expect(WMessage).to.be.exist
   })
 
-  describe('props ', () => {
+  describe('props ', function() {
     const Constructor = Vue.extend(WMessage)
     let vm
     afterEach(() => {
       vm.$destroy()
     })
 
-    it('可以设置 content', () => {
-      vm = new Constructor({
-        propsData: {
-          content: 'test'
-        }
-      }).$mount()
-      const spanContent = vm.$el.querySelector('.w-message-content')
-      expect(spanContent.textContent).to.equal('test')
+    it('可以设置 type', function() {
+      const typeList = ['info', 'success', 'warning', 'error']
+      typeList.forEach(type => {
+        vm = new Constructor({
+          propsData: {
+            type
+          }
+        }).$mount()
+        const useElement = vm.$el.querySelector('svg > use')
+        const xlinkHref = useElement.getAttribute('xlink:href')
+        expect(xlinkHref).to.equal(`#icon-${type}`)
+      })
     })
 
-    it('可以设置 type', () => {
-      vm = new Constructor({
-        propsData: {
-          type: 'error'
-        }
-      }).$mount()
-      const useElement = vm.$el.querySelector('svg > use')
-      const xlinkHref = useElement.getAttribute('xlink:href')
-      expect(xlinkHref).to.equal('#icon-error')
-    })
-
-    it('可以设置 icon', () => {
+    it('可以设置 icon', function() {
       vm = new Constructor({
         propsData: {
           icon: 'setting'
@@ -50,7 +43,31 @@ describe('WMessage ', () => {
       expect(xlinkHref).to.equal('#icon-setting')
     })
 
-    it('可以同时设置 type 和 icon，会显示 icon', () => {
+    it('可以设置 iconColor', function() {
+      const divElement = document.createElement('div')
+      document.body.appendChild(divElement)
+      vm = new Constructor({
+        propsData: {
+          icon: 'setting',
+          iconColor: 'red'
+        }
+      }).$mount(divElement)
+      const icon = vm.$el.querySelector('.w-message-icon')
+      expect(getComputedStyle(icon).color).to.equal('rgb(255, 0, 0)')
+      vm.$el.remove()
+    })
+
+    it('可以设置 content', function() {
+      vm = new Constructor({
+        propsData: {
+          content: 'test'
+        }
+      }).$mount()
+      const span = vm.$el.querySelector('.w-message-content')
+      expect(span.textContent).to.equal('test')
+    })
+
+    it('同时设置 type 和 icon，最终会显示 icon', function() {
       vm = new Constructor({
         propsData: {
           type: 'success',
@@ -62,23 +79,19 @@ describe('WMessage ', () => {
       expect(xlinkHref).to.equal('#icon-setting')
     })
 
-    it('可以设置 iconColor', () => {
-      const divElement = document.createElement('div')
-      document.body.appendChild(divElement)
+    it('可以设置 dangerouslyUseHTML', function() {
       vm = new Constructor({
         propsData: {
-          icon: 'setting',
-          iconColor: 'black'
+          content: '<strong id="test">test</strong>',
+          dangerouslyUseHTML: true
         }
-      }).$mount(divElement)
-      const iconWrapper = vm.$el.querySelector('.w-message-icon-wrapper')
-      expect(getComputedStyle(iconWrapper).color).to.equal('rgb(0, 0, 0)')
-      const svgElement = vm.$el.querySelector('svg')
-      expect(getComputedStyle(svgElement).color).to.equal('rgb(0, 0, 0)')
-      vm.$el.remove()
+      }).$mount()
+      const strongElement = vm.$el.querySelector('#test')
+      expect(strongElement).to.be.exist
     })
 
-    it('可以设置 duration', (done) => {
+    it('可以设置 duration', function(done) {
+      this.timeout(4000)
       const divElement = document.createElement('div')
       document.body.appendChild(divElement)
       vm = new Constructor({
@@ -86,64 +99,15 @@ describe('WMessage ', () => {
           duration: 1
         }
       }).$mount(divElement)
+      vm.visible = true
       setTimeout(() => {
         expect(document.body.contains(vm.$el)).to.be.false
         vm.$el.remove()
         done()
-      }, 1500)
+      }, 2000)
     })
 
-    it('可以设置 duration 为 0，表示 WMessage 不会自动关闭', (done) => {
-      const spy = sinon.spy()
-      vm = new Constructor({
-        propsData: {
-          duration: 0,
-          onClose () {
-            spy()
-          }
-        }
-      }).$mount()
-      setTimeout(() => {
-        expect(spy).to.have.been.not.be.called
-        done()
-      }, 1500)
-    })
-
-    it('可以设置 top', () => {
-      const divElement = document.createElement('div')
-      document.body.appendChild(divElement)
-      vm = new Constructor({
-        propsData: {
-          top: 48
-        }
-      }).$mount(divElement)
-      expect(getComputedStyle(vm.$el).top).to.equal('48px')
-      vm.$el.remove()
-    })
-
-    it('可以设置 closeable', () => {
-      vm = new Constructor({
-        propsData: {
-          closeable: true
-        }
-      }).$mount()
-      const useElement = vm.$el.querySelector('svg > use')
-      const xlinkHref = useElement.getAttribute('xlink:href')
-      expect(xlinkHref).to.equal('#icon-close')
-    })
-
-    it('可以设置 dangerouslyEnableHTML', () => {
-      vm = new Constructor({
-        propsData: {
-          content: '<strong id="test">test</strong>',
-          dangerouslyEnableHTML: true
-        }
-      }).$mount()
-      const strongElement = vm.$el.querySelector('#test')
-      expect(strongElement).to.be.exist
-    })
-
-    it('可以设置 onClose', (done) => {
+    it('可以设置 onClose', function(done) {
       const spy = sinon.spy()
       vm = new Constructor({
         propsData: {
@@ -157,6 +121,50 @@ describe('WMessage ', () => {
         expect(spy).to.have.been.called
         done()
       }, 1500)
+    })
+
+    it('可以设置 closeable', function() {
+      vm = new Constructor({
+        propsData: {
+          closeable: true
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('.w-message-close > use')
+      const xlinkHref = useElement.getAttribute('xlink:href')
+      expect(xlinkHref).to.equal('#icon-close')
+    })
+
+    it('可以设置 top', function() {
+      const divElement = document.createElement('div')
+      document.body.appendChild(divElement)
+      vm = new Constructor({
+        propsData: {
+          top: 24
+        }
+      }).$mount(divElement)
+      expect(getComputedStyle(vm.$el).top).to.equal('24px')
+      vm.$el.remove()
+    })
+  })
+
+  describe('event ', function() {
+    it('支持 close', function(done) {
+      const divElement = document.createElement('div')
+      document.body.appendChild(divElement)
+      const spy = sinon.spy()
+      const Constructor = Vue.extend(WMessage)
+      const vm = new Constructor({
+        propsData: {
+          duration: 1,
+          onClose () {
+            spy()
+          }
+        }
+      }).$mount(divElement)
+      vm.$on('close', () => {
+        expect(spy).to.have.been.called
+        done()
+      })
     })
   })
 })
